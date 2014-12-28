9960580e9d4ff6cceed4cbef311a68c705e3dc13
quezixiong <575303442@qq.com>
2014-12-28 16:08:20 +0800

    fix bug of bytes and string confusion

--- redis_undigraph/__init__.py
+++ redis_undigraph/__init__.py
@@ -4,16 +4,16 @@ from redis_wrap import *
 #-------------------edges-----------------------
 def add_edge(node1, node2, system='default'):
     edges1 = get_set(node1, system=system)
-    edges1.add(node1)
+    edges1.add(node2)
     edges2 = get_set(node2, system=system)
-    edges2.add(node2)
+    edges2.add(node1)
 
 
 def delete_edge(node1, node2, system='default'):
     edges1 = get_set(node1, system=system)
-    edges1.remove(node1)
+    edges1.remove(node2)
     edges2 = get_set(node2, system=system)
-    edges2.remove(node2)
+    edges2.remove(node1)
 
 
 def has_edge(node1, node2, system='default'):
@@ -34,7 +34,8 @@ def get_edge_value(node1, node2, system='default'):
         edge_key = 'ev:'+node1+'_'+node2
     else:
         edge_key = 'ev:'+node2+'_'+node1
-    return get_redis(system).get(edge_key).decode()
+    value = get_redis(system).get(edge_key)
+    return value.decode() if value else value
 
 
 def set_edge_value(node1, node2, value, system='default'):
@@ -45,10 +46,19 @@ def set_edge_value(node1, node2, value, system='default'):
     return get_redis(system).set(edge_key, value)
 
 
+def delete_edge_value(node1, node2, system='default'):
+    if node1 > node2:
+        edge_key = 'ev:'+node1+'_'+node2
+    else:
+        edge_key = 'ev:'+node2+'_'+node1
+    return get_redis(system).delete(edge_key)
+
+
 #-------------------node value--------------------
 def get_node_value(node, system='default'):
     node_key = 'nv:%s' % node
-    return get_redis(system).get(node_key).decode()
+    value = get_redis(system).get(node_key)
+    return value.decode() if value else value
 
 
 def set_node_value(node, value, system='default'):
@@ -56,5 +66,16 @@ def set_node_value(node, value, system='default'):
     return get_redis(system).set(node_key, value)
 
 
+def delete_node_value(node, system='default'):
+    node_key = 'nv:%s' % node
+    return get_redis(system).delete(node_key)
+
+
+#-------------------node cleaning up--------------------
+def delete_node(node, system='default'):
+    for neighbor in neighbors(node):
+        delete_edge_value(node, neighbor)
+    delete_node_value(node)
+    return get_redis(system).delete(node)
 
 
